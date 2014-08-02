ed9ebd249bc2818ebf1e61137476b6c932655a65
Andy Schetter <andyschetter@gmail.com>
2014-08-01 20:20:29 -0500

    Add basic player, team, and membership models

---
+++ db/migrate/20140802010814_create_memberships.rb
@@ -0,0 +1,11 @@
+class CreateMemberships < ActiveRecord::Migration
+  def change
+    create_table :memberships do |t|
+
+      t.belongs_to :player
+      t.belongs_to :team
+
+      t.timestamps
+    end
+  end
+end
