8a0fe2aad1b43d5cace84e4ffd4f36a37f65a5b7
Andy Schetter <andyschetter@gmail.com>
2014-08-19 17:42:13 -0500

    DRY up routes code

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  ###### SEASONS ROUTES ########
-
-  resources :seasons, only: [ :index, :show ] do
+  def stats_routes
     get 'stats', :on => :member
     get 'advanced_stats', :on => :member
+  end
 
+  def financial_routes
     get 'salaries', :on => :member
     get 'win_shares', :on => :member
     get 'win_shares_index', :on => :member
@@ -15,38 +15,25 @@ Rails.application.routes.draw do
     get 'cost_per_rebound', :on => :member
     get 'cost_per_block', :on => :member
     get 'cost_per_minute', :on => :member
+  end
+
+  ###### SEASONS ROUTES ########
+
+  resources :seasons, only: [ :index, :show ] do
+    stats_routes
+    financial_routes
 
   ###### TEAMS ROUTES ########
 
     resources :teams, only: [ :index, :show ] do
-      get 'stats', :on => :member
-      get 'advanced_stats', :on => :member
-
-      get 'salaries', :on => :member
-      get 'win_shares', :on => :member
-      get 'win_shares_index', :on => :member
-
-      get 'cost_per_point', :on => :member
-      get 'cost_per_assist', :on => :member
-      get 'cost_per_rebound', :on => :member
-      get 'cost_per_block', :on => :member
-      get 'cost_per_minute', :on => :member
+      stats_routes
+      financial_routes
 
   ###### PLAYERS ROUTES ########
 
       resources :players, only: [ :index, :show ] do
-        get 'stats', :on => :member
-        get 'advanced_stats', :on => :member
-
-        get 'salaries', :on => :member
-        get 'win_shares', :on => :member
-        get 'win_shares_index', :on => :member
-
-        get 'cost_per_point', :on => :member
-        get 'cost_per_assist', :on => :member
-        get 'cost_per_rebound', :on => :member
-        get 'cost_per_block', :on => :member
-        get 'cost_per_minute', :on => :member
+        stats_routes
+        financial_routes
       end
     end
   end
