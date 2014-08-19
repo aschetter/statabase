1122c733e9fe0544772811caf30eb133005dd97b
Andy Schetter <andyschetter@gmail.com>
2014-08-18 20:15:17 -0500

    Successful adjustment of player salaries

--- lib/tasks/adjustPlayerSalaries.rb
+++ lib/tasks/adjustPlayerSalaries.rb
@@ -8,10 +8,14 @@ def adjustPlayerSalaries(percentages)
 
       db_membership = Membership.find(membership[:membership_id])
 
-      db_membership.salary = total_salary * gp_percentage
+      player = Player.find(db_membership.player_id)
+      team = Team.find(db_membership.team_id)
+      season = Season.find(db_membership.season_id)
 
-      db_membership.salary
+      db_membership.salary = total_salary * gp_percentage
       db_membership.save
+
+      puts "PLAYER: #{player.name}, TEAM: #{team.br_id}, SEASON: #{season.year}, NEW SALARY: #{db_membership.salary}"
     end
   end
 end
