f8fb5fd67ec67f9edb66838455526d27bdb4d217
Andy Schetter <andyschetter@gmail.com>
2014-08-21 15:02:49 -0500

    Correct loading problem with players that don't have an online unique id

--- lib/tasks/populate/parseData/parseSalaries.rb
+++ lib/tasks/populate/parseData/parseSalaries.rb
@@ -7,14 +7,21 @@ module BBall
     salaries = htmlPage[:salaries]
 
     salaries.each do |player|
-      salary_info = player.css('td')
-      href = salary_info[1].css('a').attr("href").value
 
-      player_salary = {
-        br_id: href.slice(11..(href.index('.')-1)),
-        name: salary_info[1].css('a').text,
-        salary: salary_info[2].attr("csk").to_i
-      }
+      a_tag = salary_info[1].css('a')
+
+      if a_tag 
+
+        salary_info = player.css('td')
+        href = salary_info[1].css('a').attr("href").value
+
+        player_salary = {
+          br_id: href.slice(11..(href.index('.')-1)),
+          name: salary_info[1].css('a').text,
+          salary: salary_info[2].attr("csk").to_i
+        }
+
+      end
 
       @player_salaries << player_salary
     end
